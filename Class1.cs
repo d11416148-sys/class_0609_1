30827150dd86ffd70bfe211e0f991572f2605b61
d11416148-sys <d11416148@gs.takming.edu.tw>
2026-06-09 12:57:45 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+delegate int MyDel(int a, int b);
+
+delegate void MyDel2(int a, int b);
+
+
+public class MyMath
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Sub(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Mul(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Div(int a, int b)
+    {
+        return a / b;
+    }
+
+
+    public static string str;
+
+    public static void Add2(int a, int b)
+    {
+        str += "\n" + (a + b).ToString();
+    }
+
+    public static void Sub2(int a, int b)
+    {
+        str += "\n" + (a - b).ToString();
+    }
+
+    public static void Mul2(int a, int b)
+    {
+        str += "\n" + (a * b).ToString();
+    }
+
+    public static void Div2(int a, int b)
+    {
+        str += "\n" + (a / b).ToString();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Exercise_0609_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDel hander;
+
+            hander = new MyDel(MyMath.Add);
+
+            Console.WriteLine(hander(10, 20));
+
+            hander = new MyDel(MyMath.Sub);
+            Console.WriteLine(hander(10, 20));
+
+            hander = new MyDel(MyMath.Mul);
+            Console.WriteLine(hander(10, 20));
+
+            hander = new MyDel(MyMath.Div);
+            Console.WriteLine(hander(10, 20));
+
+            MyDel2 hander2;
+
+            hander2 = new MyDel2(MyMath.Add2);
+
+            hander2 += new MyDel2(MyMath.Sub2);
+
+            hander2 += new MyDel2(MyMath.Mul2);
+
+            hander2 += new MyDel2(MyMath.Div2);
+
+            hander2(10, 20);
+
+            Console.WriteLine(MyMath.str);
+
+
+            hander2 -= new MyDel2(MyMath.Sub2);
+
+            hander2 -= new MyDel2(MyMath.Div2);
+
+            MyMath.str = "";
+
+            hander2(10, 20);
+
+            Console.WriteLine(MyMath.str);
+        }
+    }
+}
